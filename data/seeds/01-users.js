0b3897f563ee0af08204df7c7be21c567c41ee5f
Alex <omtmn@icloud.com>
2021-09-20 19:52:36 -0400

    wip

--- data/seeds/01-users.js
+++ data/seeds/01-users.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex('users').truncate() // for Postgres: return knex.raw(TRUNCATE TABLE users CASCADE)
+  return knex('users').del() // for Postgres: return knex.raw(TRUNCATE TABLE users CASCADE)
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
